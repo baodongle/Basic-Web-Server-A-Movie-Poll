de582de11c103a7f88ecc0cae7027bea23293714
baodongle <baodongle@gmail.com>
2019-07-06 13:56:22 +0700

    Create some automated tests

--- pythonweb/IntekMovieFridayPoll/views.py
+++ pythonweb/IntekMovieFridayPoll/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -12,14 +13,25 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     """Displays a question text, with no results but with a form to vote."""
     model = Question
     template_name = 'IntekMovieFridayPoll/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
