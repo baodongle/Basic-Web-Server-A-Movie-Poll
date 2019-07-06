87fffaddd79ca2dc60e0db93615f285a693ee629
baodongle <baodongle@gmail.com>
2019-07-06 12:07:46 +0700

    Simple form processing and cutting down our code
    - Update detail template (“detail.html”) with a simple form
    - Convert poll app to use the generic views system

--- pythonweb/IntekMovieFridayPoll/views.py
+++ pythonweb/IntekMovieFridayPoll/views.py
@@ -1,26 +1,48 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-
-def index(request):
-    """The home page displays the latest few questions."""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'IntekMovieFridayPoll/index.html', context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
+class IndexView(generic.ListView):
+    """The home page for Intek Movie Friday Poll app"""
+    template_name = 'IntekMovieFridayPoll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
     """Displays a question text, with no results but with a form to vote."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'IntekMovieFridayPoll/detail.html', {'question': question})
+    model = Question
+    template_name = 'IntekMovieFridayPoll/detail.html'
 
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
     """Displays results for a particular question."""
-    return render(request, f"You're looking at the results of question {question_id}.")
+    model = Question
+    template_name = 'IntekMovieFridayPoll/results.html'
 
 
 def vote(request, question_id):
     """Handles voting for a particular choice in a particular question."""
-    return render(request, f"You're voting on question{question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'IntekMovieFridayPoll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('IntekMovieFridayPoll:results', args=(question.id,)))
